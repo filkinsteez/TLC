7efea051d24d43c3f212e2f267041643dc63967d
Eric Filkins <etfilkin@syr.edu>
2013-12-18 16:14:36 -0800

    sendgrid shiet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,12 +62,13 @@ TLC::Application.configure do
   config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtpout.secureserver.net",
-    :port                 => 80,
-    :domain               => "tlcdentalsyr.com",
-    :user_name            => "smile@tlcdentalsyr.com",
-    :password             => "4MightyMolar",
-    :authentication       => :plain,
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['app19514644@heroku.com'],
+    :password => ENV['Suy3wbvvb'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
     :enable_starttls_auto => true,
     :openssl_verify_mode => 'none' 
   }
