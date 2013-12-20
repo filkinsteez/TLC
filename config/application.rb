ea1d8d3072320c1bf51870c3c470131561698c89
Eric Filkins <etfilkin@syr.edu>
2013-12-19 22:55:40 -0800

    added config settings

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,26 @@ module TLC
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
+
+  config.action_mailer.default_url_options = { :host => 'tlcdentalsyr.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtpout.secureserver.net",
+    :port                 => 80,
+    :domain               => "tlcdentalsyr.com",
+    :user_name            => "smile@tlcdentalsyr.com",
+    :password             => "4MightyMolar",
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = {
+    :host => "tlcdentalsyr.com"
+  } 
     config.action_mailer.smtp_settings = {
       :address              => "smtpout.secureserver.net",
       :port                 => 80,
