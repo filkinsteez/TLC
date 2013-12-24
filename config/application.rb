29d0b54f3a0c9df8c91bee3cc16a3ea450ed0066
Eric Filkins <etfilkin@syr.edu>
2013-12-23 18:20:25 -0800

    precompile shit

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ end
 
 module TLC
   class Application < Rails::Application 
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
